f45673b990da8431ea08dc3dee2e5edc0fba35ca
slashlion <adrianjleong@gmail.com>
2020-11-19 20:25:04 +0000

    Delete settings.py

--- settings.py
+++
@@ -1,6 +0,0 @@
-
-#pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\slash\\miniconda3\\envs\\receptor-mensajes\\Library\\bin\\tesseract.exe'
-
-api_id = 2351345  # Client's API ID
-api_hash = 'a1a55fcc6b88f4741e75b5b9e29210bb'  # Client's API hash
-phone_id = '+5491132305926'  # Client's phone
